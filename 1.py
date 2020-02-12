42c1cad3a5d221399b6d56bc8d048126f4c410da
uditiarora <uditiarora@gmail.com>
2020-02-12 21:50:34 +0530

    so far so good

--- 1.py
+++ 1.py
@@ -64,6 +64,60 @@ def select_basic(string):
 		list1.append('$' + theta + '(' + arr[0] + ')' + '-' + arr[1] )
 	print(list1)
 
+def project_case_one(string):
+	if(string.count('%')==0):
+		return string[0: string.index(')')]
+	else :
+		while(string[0] == '(' and string[len(string)-1] == ')'):
+			string = string.strip('(')
+			string = string.strip(')')
+			string = string.strip(" ")
+		return project_case_one(string[string.index('(')+1: ])
+def project_basic(string):
+	list1 = [string]
+	string = string.strip(" ")
+	while(string[0] == '(' and string[len(string)-1] == ')'):
+		string = string.strip('(')
+		string = string.strip(')')
+	if(string.count('%')>1):
+		list1.append(string[0: string.index('(')] + '(' + project_case_one(string) + ')')
+	elif(string.find('UNION')!=-1 and string.find('#')!=-1):
+		string = string.strip('%')
+		string = string.strip(" ")
+		theta = string[0:string.index('(')]
+		inner_string = string[string.index('(')+1: -1]		
+		arr = theta.split('UNION')
+		arr2 = inner_string.split('#')
+		
+		list1.append('(' + '%' + arr[0] + '(' + arr2[0] + ')' + ')' + '#' + arr2[1][0 : arr2[1].index('E')] + '(' + '%' + arr[1] + '(' + arr2[1][arr2[1].index('E') : ] + ')' + ')' )
+		
+	print(list1)
+
+def join_basic(string):
+	list1 = [string]
+	while(string[0] == '(' and string[len(string)-1] == ')'):
+		string = string.strip('(')
+		string = string.strip(')')
+	if(string.count('#') == 1):
+		print("hi")
+		arr = string.split('#')
+		list1.append(arr[1][arr[1].index('E'): ] +' #' + arr[1][0 : arr[1].index('E')] + arr[0])
+	print(list1)
+
+def set_basic(string):
+	list1 = [string]
+	while(string[0] == '(' and string[len(string)-1] == ')'):
+		string = string.strip('(')
+		string = string.strip(')')
+	if(string.count('UNION') == 1):
+		arr = string.split('UNION')
+		list1.append(arr[1] + ' UNION ' + arr[0])
+	if(string.count('INTER') == 1):
+		arr = string.split('INTER')
+		list1.append(arr[1] + ' INTER ' + arr[0])
+	print(list1)
+		
+
 def print_equivalents(string):
 	print(string)
 
@@ -74,5 +128,5 @@ if __name__ == "__main__":
 		sample = sample.strip("\n")
 		sample = sample.strip("\t")
 		sample = sample.strip(" ")
-		select_basic(sample);
+		set_basic(sample);
 	fo.close()
